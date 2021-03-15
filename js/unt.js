b0ef14f2e561dd02049eee278eb2995219301eff
Fleyteug <@Fleyto41@mail.ru>
2021-03-15 21:36:04 +0300

    testCommit1

---
+++ js/unt.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    console.log('Ну как?');
+}
+
+function showMessage2() {
+    console.log('получилось?');
+}
+
+
+function initMessages() {
+    showMessage();
+    showMessage2();
+}
+
+initMessages();
